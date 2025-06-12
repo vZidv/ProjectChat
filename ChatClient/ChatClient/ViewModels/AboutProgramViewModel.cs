3ad3be115d551fdea2f81cf08747217f7b6af804
vZidv <diz.shulinus@yandex.ru>
2025-06-12 21:03:47 +0300

    feat: ChatClient add page "AboutProgram"

---
+++ ChatClient/ChatClient/ViewModels/AboutProgramViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatClient.ViewModels
+{
+    class AboutProgramViewModel
+    {
+        //Fields
+
+        //Properties
+
+        //Commands
+        
+        public ICommand ClosePageCommand { get; }
+
+        //urlCommands
+
+        public ICommand LicenseUrlCommand { get; }
+        public ICommand GithubUrlCommand { get; }
+        public ICommand ProfGithubUrlCommand { get; }
+        //public ICommand ProfGithubUrlCommand { get; }
+        //public ICommand ProfGithubUrlCommand { get; }
+
+        public AboutProgramViewModel()
+        {
+            ClosePageCommand = new ViewModelCommand(ExecuteClosePageCommand);
+
+            LicenseUrlCommand = new ViewModelCommand(ExecuteLicenseUrlCommand);
+            GithubUrlCommand = new ViewModelCommand(ExecuteGithubUrlCommand);
+            ProfGithubUrlCommand = new ViewModelCommand(ExecuteProfGithubUrlCommand);
+        }
+
+        private void ExecuteProfGithubUrlCommand(object? obj) => OpenUrl("https://github.com/vZidv");
+
+        private void ExecuteGithubUrlCommand(object? obj) => OpenUrl("https://github.com/vZidv/ProjectChat.git");
+
+        private void ExecuteLicenseUrlCommand(object? obj) => OpenUrl("https://github.com/vZidv/ProjectChat.git");
+
+
+        private void ExecuteClosePageCommand(object? obj) => Services.NavigationService.TopFrame.Content = null;
+
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                ProcessStartInfo psi = new()
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex) { }
+        }
+
+    }
+}
--- ChatClient/ChatClient/ViewModels/LeftBorderMenuViewModel.cs
+++ ChatClient/ChatClient/ViewModels/LeftBorderMenuViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ChatClient.CustomControls;
+using ChatClient.View;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,14 +22,24 @@ namespace ChatClient.ViewModels
 
         public ICommand OpenProjectLinkCommand { get; }
         public ICommand OpenProjectVersionLinkCommand { get; }
+        public ICommand OpenAboutProgramCommand { get; }
 
         public LeftBorderMenuViewModel()
         {
             ClosePageCommand = new ViewModelCommand(ExecuteClosePageCommand);
             OpenProjectLinkCommand = new ViewModelCommand(ExecuteOpenProjectLinkCommand);
             OpenProjectVersionLinkCommand = new ViewModelCommand(ExecuteOpenProjectVersionLinkCommand);
+            OpenAboutProgramCommand = new ViewModelCommand(ExecuteOpenAboutProgramCommand);
         }
 
+        private void ExecuteOpenAboutProgramCommand(object? obj)
+        {
+            AboutProgramView view = new();
+            AboutProgramViewModel viewModel = new();
+            view.DataContext = viewModel;
+
+            Services.NavigationService.TopFrame.Content = view;
+        }
 
         private void ExecuteClosePageCommand(object? obj) => Services.NavigationService.TopFrame.Content = null;
 
