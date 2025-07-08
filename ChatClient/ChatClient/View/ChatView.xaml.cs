9035164e2794cd518d045b48e096259b9a08b069
vZidv <diz.shulinus@yandex.ru>
2025-07-08 14:40:26 +0300

    feat: add chat auto scroll

--- ChatClient/ChatClient/View/ChatView.xaml.cs
+++ ChatClient/ChatClient/View/ChatView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,69 @@ namespace ChatClient.View
     /// </summary>
     public partial class ChatView : Page
     {
+        private INotifyCollectionChanged? _lastCollection;
+
         public ChatView()
         {
             InitializeComponent();
+        }
+
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ChatClient.ViewModels.ChatViewModel vm)
+            {
+                if (_lastCollection != null)
+                    _lastCollection.CollectionChanged -= ChatMessages_CollectionChanged;
+
+                if (vm.ChatMessageDTOs is INotifyCollectionChanged notifyCollection)
+                {
+                    notifyCollection.CollectionChanged += ChatMessages_CollectionChanged;
+                    _lastCollection = notifyCollection;
+                }
+
+                ScrollToLastMessage();
+            }
+        }
+
+        private void ScrollToLastMessage()
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (message_listBox.Items.Count > 0)
+                {
+                    message_listBox.ScrollIntoView(message_listBox.Items[message_listBox.Items.Count - 1]);
+                }
+            }));
+        }
+
+        private void ChatMessages_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                var scrollViewer = GetScrollViewer(message_listBox);
+                if (scrollViewer != null)
+                {
+                    bool isAtBottom = scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - 10;
+                    if (isAtBottom && message_listBox.Items.Count > 0)
+                    {
+                        message_listBox.ScrollIntoView(message_listBox.Items[message_listBox.Items.Count - 1]);
+                    }
+                }
+            }));
+        }
+
+        private static ScrollViewer? GetScrollViewer(DependencyObject depObj)
+        {
+            if (depObj is ScrollViewer) return (ScrollViewer)depObj;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                var result = GetScrollViewer(child);
+                if (result != null) return result;
+            }
+            return null;
         }
     }
 }
